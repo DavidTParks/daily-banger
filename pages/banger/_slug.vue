9e7d5f69f8181f995c9dd9895fe9018086263711
David Parks <davidthomasparks@gmail.com>
2019-08-21 21:03:08 -0700

    head SEO fixes

--- pages/banger/_slug.vue
+++ pages/banger/_slug.vue
@@ -29,76 +29,34 @@ export default {
   async fetch({ store, params }) {
     await store.dispatch('banger/getBangerBySlug', params.slug)
   },
-  // head () {
-  //   return {
-  //     title: `${this.banger.songTitle} by ${this.banger.artist} - The Daily Banger`,
-  //     meta: [
-  //       { hid: 'description', name: 'description', content: this.banger.articleContent },
-  //       {
-  //         hid: `keywords`,
-  //         name: 'keywords',
-  //         content: `${this.banger.songTitle}, ${this.banger.artist}, ${this.banger.genre}`
-  //       },
-  //       //Open Graph
-  //       { name: 'og:title', content: `${this.banger.songTitle} by ${this.banger.artist} - The Daily Banger` },
-  //       { name: 'og:description', content: this.banger.articleContent },
-  //       { name: 'og:image', content: this.banger.songImage.url },
-  //       { name: 'og:url', content: `https://www.thedailybanger.com/banger/${this.$route.params.slug}` },
-  //       { name: 'og:type', content: 'article' },
-
-  //       // Twitter Card
-  //       { name: 'twitter:card', content: 'summary_large_image'},
-  //       { name: 'twitter:site', content: '@twitter' },
-  //       { name: 'twitter:title', content: `${this.banger.songTitle} by ${this.banger.artist} - The Daily Banger` },
-  //       { name: 'twitter:description', content: this.banger.articleContent },
-  //       { name: 'twitter:image', content: this.banger.songImage.url },
-  //       { name: 'twitter:image:alt', content: `Song cover for ${this.banger.songTitle} by ${this.banger.artist}` }
-  //     ]
-  //   }
-  // },
-  // apollo: {
-  //   banger: {
-  //     query: gql`query banger($slug: String!) {
-  //       banger(filter: {urlSlug: {
-  //         eq: $slug
-  //       } }) {
-  //         artist
-  //         date
-  //         id
-  //         songTitle
-  //         songImage {
-  //           url
-  //         }
-  //         soundcloudLink
-  //         articleContent
-  //         urlSlug
-  //         genre
-  //       }
-  //     }`,
-  //     preFetch({ route }) {
-  //       return {
-  //         slug: route.params.slug
-  //       }
-  //     },
-  //     variables() {
-  //       return {
-  //         slug: this.slug
-  //       }
-  //     },
-  //     result({data}) {
-  //       console.log(data);
-  //     }
-  //   }
-  // },
-  layout: 'default',
-  mounted: function() {
-    console.log(this.$route.params.slug);
-  },
-  data() {
+  head () {
     return {
-      slug: this.$route.params.slug
+      title: `${this.currentBanger.fields.songTitle} by ${this.currentBanger.fields.artist} - The Daily Banger`,
+      meta: [
+        { hid: 'description', name: 'description', content: this.currentBanger.fields.articleContent.content[0].content[0].value },
+        {
+          hid: `keywords`,
+          name: 'keywords',
+          content: `${this.currentBanger.fields.songTitle}, ${this.currentBanger.fields.artist}, ${this.currentBanger.fields.genres[0]}`
+        },
+        //Open Graph
+        { name: 'og:title', content: `${this.currentBanger.fields.songTitle} by ${this.currentBanger.fields.artist} - The Daily Banger` },
+        { name: 'og:description', content: this.currentBanger.fields.articleContent.content[0].content[0].value },
+        { name: 'og:image', content: this.currentBanger.fields.image.fields.file.url },
+        { name: 'og:url', content: `https://www.thedailybanger.com/banger/${this.$route.params.slug}` },
+        { name: 'og:type', content: 'article' },
+
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary_large_image'},
+        { name: 'twitter:site', content: '@twitter' },
+        { name: 'twitter:title', content: `${this.currentBanger.fields.songTitle} by ${this.currentBanger.fields.artist} - The Daily Banger` },
+        { name: 'twitter:description', content: this.currentBanger.fields.articleContent.content[0].content[0].value },
+        { name: 'twitter:image', content: this.currentBanger.fields.image.fields.file.url },
+        { name: 'twitter:image:alt', content: `Song cover for ${this.currentBanger.fields.songTitle} by ${this.currentBanger.fields.artist}` }
+      ]
     }
-  }
+  },
+  layout: 'default',
 }
 </script>
 
